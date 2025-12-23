5f022a5854f95173da163df56d00bcbd307e5308
David Mwangi <intelligentsoftwaresdev@gmail.com>
2025-12-23 17:54:20 +0300

    feat: Add PostHog integration with environment variables and update dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { PostHogProvider } from 'posthog-js/react'
+    
 import App from "./App";
 import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
+createRoot(document.getElementById("root")!).render(<StrictMode>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <App />
+    </PostHogProvider>
+  </StrictMode>);
